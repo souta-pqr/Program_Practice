d260811e50e928adea561b2fc5ff06e1b0ff06e1
souta-pqr <boriko0116@gmail.com>
2026-05-04 10:21:19 +0900

    python script

---
+++ python/python-benkyokai/sample.py
@@ -0,0 +1,6 @@
+name = "Alice"
+age = 22
+pi = 3.14
+ok = True
+xs = [1, 2, 3]
+print (type(age))
---
+++ python/python-benkyokai/sample2.py
@@ -0,0 +1,23 @@
+print(1+2)
+print(7/2)
+print(7//2)
+print(7%2)
+print(2**10)
+
+a = 1
+b = 2
+
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+
+x = "str"
+y = "str1"
+
+print(x and y)
+print(x or y)
+print(not y)
+
+name, age = "Alice", 22
+print(f"{name} is {age} years old.")
